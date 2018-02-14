b259bff12b4b3c1b52534752835fa028575b7c09
usuarioJapa <willian.hsimabukuro@gmail.com>
2018-02-14 02:01:48 -0200

    fixed extra comma

--- app/socket/index.js
+++ app/socket/index.js
@@ -23,7 +23,7 @@ module.exports = server => {
     })
 
     socket.on('make-answer', data => {
-      socket.to(data.to,).emit('answer-made', {
+      socket.to(data.to).emit('answer-made', {
         socket: socket.id,
         answer: data.answer
       })
